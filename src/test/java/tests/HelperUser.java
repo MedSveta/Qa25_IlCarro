d0319de48a5e172773e3c6abfb90da7ff238d024
MedSveta <sveta1978medved@gmail.com>
2024-05-02 18:13:35 +0300

    added new project

---
+++ src/test/java/tests/HelperUser.java
@@ -0,0 +1,10 @@
+package tests;
+
+import manager.HelperBase;
+import org.openqa.selenium.WebDriver;
+
+public class HelperUser extends HelperBase {
+    public HelperUser(WebDriver wd) {
+        super(wd);
+    }
+}
