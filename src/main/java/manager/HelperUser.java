2726acb8a30a8cfb8641e4f71558c3a09da7c456
MedSveta <sveta1978medved@gmail.com>
2024-05-05 18:37:03 +0300

    added loginPositiveTest

---
+++ src/main/java/manager/HelperUser.java
@@ -0,0 +1,32 @@
+package manager;
+
+import manager.HelperBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HelperUser extends HelperBase {
+    public HelperUser(WebDriver wd) {
+        super(wd);
+    }
+
+    public void openLoginForm() {
+        click(By.cssSelector("a[href='/login?url=%2Fsearch']"));
+    }
+
+    public void fillLoginForm(String email, String password){
+        type(By.xpath("//*[@id='email']"), email);
+        type(By.xpath("//*[@id='password']"), password);
+    }
+    public  void submitLogin(){
+        click(By.xpath("//button[@type='submit']"));
+    }
+    public  void closeWindowLoggedIn(){
+        click(By.xpath("//button[text()='Ok']"));
+    }
+    public boolean isLogged() {
+        return isElementPresent(By.cssSelector("a[href='/logout?url=%2Fsearch']"));
+    }
+    public void logout() {
+        click(By.cssSelector("a[href='/logout?url=%2Fsearch']"));
+    }
+}
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,25 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTests extends TestBase{
-    @Test
-    public  void  test(){
+public class LoginTests extends TestBase {
 
+    @BeforeMethod
+    public void preCondition() {
+        if (app.getHelperUser().isLogged()) {
+            app.getHelperUser().logout();
+        }
+    }
+    @Test
+    public void LoginSuccess() {
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("sveta12345@gmail.com", "1234567$Ru");
+        app.getHelperUser().submitLogin();
+        app.getHelperUser().closeWindowLoggedIn();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
     }
 
 }
